9625e1b31889d209a7e096b0f3283207acfd44da
Soumika Sahoo <sahoosoumika247@gmail.com>
2026-07-28 17:22:23 +0530

    Time: 7 ms (37.55%) | Memory: 45.9 MB (62.02%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -8,13 +8,19 @@ class Solution {
         char ch=s.charAt(high);
         map.put(ch,map.getOrDefault(ch,0)+1);
       
-while(map.get(ch)>1){
- char leftChar=s.charAt(low);
- map.put(leftChar,map.get(leftChar)-1);
- low++;
+int k=high-low+1;
+while(map.size()<k){
+     char leftchar =s.charAt(low);
+       map.put(leftchar,map.get(leftchar)-1);
+       if(map.get(leftchar)==0){
+           map.remove(leftchar);
+       }
+       low++;
+    k=high-low+1;
+   
 }
 ans=Math.max(ans,high-low+1);
-    }
-    return ans;
+}
+return ans ;
 }
 }
