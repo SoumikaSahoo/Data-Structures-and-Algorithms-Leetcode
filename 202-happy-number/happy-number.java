645d3a0d631191334d48301f740c58b70320e835
Soumika Sahoo <sahoosoumika247@gmail.com>
2026-07-30 10:02:04 +0530

    Time: 1 ms (76.88%) | Memory: 42.5 MB (34.37%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        while(fast!=1){
+            slow =func(slow);
+            fast=func(fast);
+            fast=func(fast);
+            if(slow==fast && slow!=1){
+                return false;
+            }
+        }
+        fast=1;
+        return true;
+
+    }
+    private int func(int n){
+        int sum=0;
+        while(n>0){
+            int d=n%10;
+        
+            sum=sum+d*d;
+                n=n/10;
+        }
+        return sum;
+    }
+}
