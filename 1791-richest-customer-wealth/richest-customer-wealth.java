1bc8e1592bc56c130db806e373368195d246ba3f
Soumika Sahoo <sahoosoumika247@gmail.com>
2026-08-10 18:24:07 +0530

    Time: 1 ms (16.15%) | Memory: 44.7 MB (37.86%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int Maxwealth=0;
+        for (int i=0;i<accounts.length;i++){
+            int wealth=0;
+            for(int j=0;j<accounts[0].length;j++){
+                wealth=wealth+accounts[i][j];
+                    
+            }
+            if(wealth>Maxwealth){
+                Maxwealth=wealth;
+            }
+        }
+        return Maxwealth;
+    }
+}
