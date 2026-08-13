068b209a010403a0807d89e00d91dd661d3c04f9
Soumika Sahoo <sahoosoumika247@gmail.com>
2026-08-13 19:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (75.73%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int xorOperation(int n, int start) {
         int xor=0;
         int c=0;
-        for(int i =start;c<n;i=i+2){
+        for(int i =start;i<start+2*n;i=i+2){
             xor=xor^i;
             c++;
         }
